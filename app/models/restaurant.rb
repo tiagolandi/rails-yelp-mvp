8d4772d336af8ae4dd7a0166750b6ad79a15185b
tiago landi <tiagolandi@gmail.com>
2020-09-17 13:14:01 -0300

    yelp mvp models done

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian), message: "%{value} must be either, chinese, italian, japanese, french or belgian" }
+end
